58a34cedd11e59db1ef8029dd1bb5af89e0979e3
nikita_balakhontsev <nikita.balakhontsev@gmail.com>
2024-05-02 11:11:18 +0300

    fix logging

--- logging_setup.py
+++ logging_setup.py
@@ -4,6 +4,7 @@ import json
 import os
 from datetime import datetime as datetime
 
+
 def setup_logging(config_path: str = 'logging_config.json', log_path: Path = Path('log')):
     config_file = Path(config_path)
     with open(config_file) as f_in:
@@ -12,9 +13,13 @@ def setup_logging(config_path: str = 'logging_config.json', log_path: Path = Pat
     if not os.path.exists(log_path):
         os.mkdir(log_path)
 
-    config['handlers']['error_file_handler']['filename'] = Path(log_path, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_errors.log')
-    config['handlers']['info_file_handler']['filename'] = Path(log_path, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_info.log')
-    config['handlers']['payment_file_handler']['filename'] = Path(log_path, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_payments.json')
+    error_path = Path(log_path, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_errors.log')
+    info_path = Path(log_path, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_info.log')
+    payment_path = Path(log_path, f'{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_payments.json')
+
+    config['handlers']['error_file_handler']['filename'] = error_path
+    config['handlers']['info_file_handler']['filename'] = info_path
+    config['handlers']['payment_file_handler']['filename'] = payment_path
 
     logging.addLevelName(15, "PAYMENT")
     logging.config.dictConfig(config)
