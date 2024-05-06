b38352bc7c2c8e632f440013f681320327431bde
nikita_balakhontsev <nikita.balakhontsev@gmail.com>
2024-05-06 22:22:14 +0300

    zodiac signs only in Russian

--- bot/config.py
+++ bot/config.py
@@ -1,18 +1,7 @@
 data_path = r'data/'
 
-ZODIAC_SIGNS = [
-    {'ru': 'Овен', 'en': 'aries'},
-    {'ru': 'Телец', 'en': 'taurus'},
-    {'ru': 'Близнецы', 'en': 'gemini'},
-    {'ru': 'Рак', 'en': 'cancer'},
-    {'ru': 'Лев', 'en': 'leo'},
-    {'ru': 'Дева', 'en': 'virgo'},
-    {'ru': 'Весы', 'en': 'libra'},
-    {'ru': 'Скорпион', 'en': 'scorpio'},
-    {'ru': 'Стрелец', 'en': 'sagittarius'},
-    {'ru': 'Козерог', 'en': 'capricorn'},
-    {'ru': 'Водолей', 'en': 'aquarius'},
-    {'ru': 'Рыбы', 'en': 'pisces'},
-]
+ZODIAC_SIGNS = ['Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева',
+                'Весы', 'Скорпион', 'Стрелец', 'Козерог', 'Водолей', 'Рыбы']
 
-ZODIAC_BTNS = {sign['ru']: sign['ru'] for sign in ZODIAC_SIGNS}
+
+ZODIAC_BTNS = {sign: sign for sign in ZODIAC_SIGNS}
